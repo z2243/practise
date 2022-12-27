57c603d2d5899b5d7b80e97cd6a112a6c09f3cae
itcast <2243240705@qq.com>
2022-12-27 21:04:55 +0800

    UnicodePractise

--- src/Unicode/UnicodePractise.java
+++ src/Unicode/UnicodePractise.java
@@ -2,7 +2,13 @@ package Unicode;
 
 public class UnicodePractise {
     public static void main(String[] args) {
-        String sentence = "⏰时钟";
+        String sentence = "\uD800时钟";
+        int i = sentence.codePointAt(0);
+        char c = sentence.charAt(0);
+        System.out.println(c);
+        System.out.println(i);
+        //用于确定指定字符(Unicode代码点)是否在补充字符范围内
+        System.out.println(Character.isSupplementaryCodePoint(i));
 
     }
 }
