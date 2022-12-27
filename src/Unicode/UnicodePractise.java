c64da5266f0c3f174e23650cbeaa3561bb84f0ea
itcast <2243240705@qq.com>
2022-12-27 21:13:17 +0800

    UnicodePractise

--- src/Unicode/UnicodePractise.java
+++ src/Unicode/UnicodePractise.java
@@ -5,10 +5,10 @@ public class UnicodePractise {
         String sentence = "\uD800时钟";
         int i = sentence.codePointAt(0);
         char c = sentence.charAt(0);
-        System.out.println(c);
-        System.out.println(i);
+        System.out.println(c);//? 输出字符
+        System.out.println(i);//55296 输出码点
         //用于确定指定字符(Unicode代码点)是否在补充字符范围内
-        System.out.println(Character.isSupplementaryCodePoint(i));
+        System.out.println(Character.isSupplementaryCodePoint(i));//false //表示55926不在‘补充字符’的范围内
 
     }
 }
