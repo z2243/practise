d42a55fc9aba68c484f66cad1626e5dfd0c82e74
itcast <2243240705@qq.com>
2022-12-30 16:33:44 +0800

    WhilePractise

---
+++ src/LogicalStatement/IfPratise.java
@@ -0,0 +1,7 @@
+package LogicalStatement;
+
+public class IfPratise {
+    public static void main(String[] args) {
+        //计算Lottery彩票的几率odds
+    }
+}
--- src/LogicalStatement/LogicalStatement.java
+++ src/LogicalStatement/LogicalStatement.java
@@ -1,3 +1,5 @@
+package LogicalStatement;
+
 import java.util.Scanner;
 
 public class LogicalStatement {
